a11abdf04744909234dbc8d66f3ede414d2a3daf
N <66836085+nelscodes@users.noreply.github.com>
2026-07-11 19:08:45 +0800

    Update script.js

--- include/script.js
+++ include/script.js
@@ -24,3 +24,47 @@ function copyEmail(element) {
       }, 2000);
     });
 }
+
+//hover img//
+function mediaQueryCheck(inputQuery) {
+  var content = document.getElementById("navigation");
+  if (inputQuery.matches) {
+    for (let el of document.querySelectorAll(".hover")) {
+      let image = new Image();
+      image.src = el.dataset.src;
+      image.className = "followMouse";
+      image.style.width = "240px";
+      image.style.position = "fixed";
+
+      el.addEventListener("mouseover", (e) => {
+        document.getElementById("container").append(image);
+        image.style.left = `${e.clientX * 1.1}px`;
+        image.style.top = `${e.clientY * 1.05}}px`;
+      });
+
+      el.addEventListener("mouseout", (e) => {
+        image.remove();
+      });
+    }
+
+    window.addEventListener("mousemove", (e) => {
+      let image = document.querySelector(".followMouse");
+      if (image) {
+        image.style.left = `${e.clientX * 1.1}px`;
+        image.style.top = `${e.clientY * 1.05}px`;
+      }
+    });
+  } else {
+  }
+}
+var mobileQuery = window.matchMedia("(min-width: 900px)");
+mediaQueryCheck(mobileQuery);
+mobileQuery.addListener(mediaQueryCheck);
+
+//footer year//
+document.addEventListener("DOMContentLoaded", () => {
+  const yearEl = document.getElementById("year");
+  if (yearEl) {
+    yearEl.textContent = new Date().getFullYear();
+  }
+});
